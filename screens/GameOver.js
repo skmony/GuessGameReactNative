ec71d93324a81a6cf21fb1aeec91a2ed0282cd2e
Shashikant Mony <shashikantmony@gmail.com>
2022-05-15 18:35:46 +0530

    Modify Game Over page

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -1,6 +1,43 @@
-import { Text } from "react-native";
+import { View, Image, StyleSheet, Text } from "react-native";
+import Title from "../components/UI/Title";
+import Colors from "../constants/colors";
 function GameOver() {
-  return <Text> Game is Over</Text>;
+  return (
+    <View style={styles.rootContainer}>
+      <Title>Game Over</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
+      <View>
+        <Text>Your phone nneded X times to guess the number Y</Text>
+      </View>
+    </View>
+  );
 }
 
 export default GameOver;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
